b550734e8147713fbc1bd09f70ea57106898b8b1
Hayato Tanaka <nkc.tanaka.hayato@gmail.com>
2017-10-26 10:18:40 +0900

    プレハブを作成

---
+++ Assets/BallContolloler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallContolloler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "DeleteSphere")
+        {
+
+            DestroyObject(collision.gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
